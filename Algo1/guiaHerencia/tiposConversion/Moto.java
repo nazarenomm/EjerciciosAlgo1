f5a3198f19928f279f5cf1ed37e6918606e3e0fe
nazarenomm <134980517+nazarenomm@users.noreply.github.com>
2023-09-27 20:38:26 -0300

    Delete Algo1 directory

--- Algo1/guiaHerencia/tiposConversion/Moto.java
+++
@@ -1,7 +0,0 @@
-package tiposConversion;
-
-class Moto extends Vehiculo {
-    void acelerar(int v) {
-        System.out.println("Moto acelerando a " + v);
-    }
-}
--- Algo1/guiaHerencia/tiposConversion/Vehiculo.java
+++
@@ -1,7 +0,0 @@
-package tiposConversion;
-
-class Vehiculo {
-    void acelerar(int velocidad) {
-        System.out.println("Vehiculo acelerando a " + velocidad);
-    }
-}
